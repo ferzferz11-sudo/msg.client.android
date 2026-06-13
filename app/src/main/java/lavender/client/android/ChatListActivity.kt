e8e3f4ad7a2cbdfd837bf935e29c244223964f60
Ferz <ferz@lavender.local>
2026-06-13 10:22:12 +0000

    fix: restore missing imports (Spinner, ArrayAdapter, ImageView, etc.)

--- app/src/main/java/lavender/client/android/ChatListActivity.kt
+++ app/src/main/java/lavender/client/android/ChatListActivity.kt
@@ -19,8 +19,14 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ProgressBar
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
