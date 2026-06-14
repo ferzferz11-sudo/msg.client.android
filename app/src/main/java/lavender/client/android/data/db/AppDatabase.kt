36cb2a6c99081d904410062a69a562551987889a
Ferz <ferz@lavender.local>
2026-06-14 17:22:59 +0000

    fix: replace deprecated fallbackToDestructiveMigration with fallbackToDestructiveMigrationOnDowngrade

--- app/src/main/java/lavender/client/android/data/db/AppDatabase.kt
+++ app/src/main/java/lavender/client/android/data/db/AppDatabase.kt
@@ -159,7 +159,7 @@ abstract class AppDatabase : RoomDatabase() {
                     "lavender_cache"
                 )
                 .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6, MIGRATION_6_7, MIGRATION_7_8, MIGRATION_8_9)
-                .fallbackToDestructiveMigration()
+                .fallbackToDestructiveMigrationOnDowngrade()
                 .build()
                 INSTANCE = instance
                 instance
